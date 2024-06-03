e0434afbc1272ac8600559ff2a0bfff43d4b229f
asad0411 <Muhammad.Asad@pixeledge.io>
2024-06-03 15:08:31 +0500

    update file accordingly to ci/cd pipeline test

--- routes/api.php
+++ routes/api.php
@@ -24,5 +24,5 @@ Route::group(['middleware' => 'auth:sanctum', 'prefix' => 'v1', 'as' => 'api.v1.
     // Keep in mind that prefix should be /api/v1.
     // Add ONE line to assign 5 resource routes to TaskController
     // Put one code line here below
-    Route::apiResource('tasks',TaskController::class);
+    Route::apiResource('tasks',TaskController::class)->names('tasks');
 });
